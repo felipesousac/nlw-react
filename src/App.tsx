b5d46287011003018bd9f1e5b899587a7ddd9bf8
felipesousac <claudioflsousa@gmail.com>
2024-02-10 18:42:58 -0300

    Render new note after its saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/NewNoteCard";
 import { NoteCard } from "./components/NoteCard";
-
-let note = {
-  date: new Date(),
-  content: "This was brought by props",
-};
+import { useState } from "react";
 
 export function App() {
+  const [notes, setNotes] = useState([
+    { id: 1, date: new Date(), content: "nota 1" },
+    { id: 2, date: new Date(), content: "nota 2" },
+  ]);
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: Math.random(),
+      date: new Date(),
+      content,
+    };
+
+    setNotes([newNote, ...notes]);
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 ">
       <img src={logo} alt="nlw expert logo" />
@@ -21,9 +32,11 @@ export function App() {
       </form>
       <div className="h-px bg-slate-700" /> {/*Line separator*/}
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard note={note} />
-        <NoteCard note={note} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {notes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
+        })}
       </div>
     </div>
   );
--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -3,7 +3,11 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
   const [content, setContent] = useState("");
 
@@ -21,6 +25,9 @@ export function NewNoteCard() {
 
   function handleSaveNote(e: FormEvent) {
     e.preventDefault();
+
+    onNoteCreated(content);
+
     setContent("");
     setShouldShowOnboarding(true);
 
